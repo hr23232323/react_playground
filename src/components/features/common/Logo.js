228217d53f94075ca6026d7202c55b27808c3a86
Harsh Rana <hr23232323@gmail.com>
2021-02-14 22:02:15 -0500

    adding basic comps to create a navbar

---
+++ src/components/features/common/Logo.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Box, Text } from "@chakra-ui/react"
+
+function Logo(){
+    return(
+        <Box>
+            <Text fontSize="lg" fontWeight="bold">
+                LOGO
+            </Text>
+        </Box>
+    )
+
+}
+
+export default Logo;
